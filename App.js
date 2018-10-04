f2d61c44a9291ae18e12075ecee0e6ebcd918a0f
Evelyn <evelynlaurindo@1sti.com.br>
2018-10-04 17:12:47 -0300

    Matches

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 
-import RegisterScreen from './source/screens/registerScreen'
-import PasswordScreen from './source/screens/passwordScreen'
+import RegisterScreen from './source/screens/registerScreen';
+import PasswordScreen from './source/screens/passwordScreen';
+import MessageScreen from './source/screens/messageScreen';
+import MatchesScreen from './source/screens/matchesScreen';
 
 export default class App extends React.Component {
   render() {
     return (
-      <RegisterScreen />
+      <MatchesScreen />
     );
   }
 }
